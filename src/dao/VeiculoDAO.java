31e34683e2024a9389ecb8ed2f881089298698a7
warlley <euwarlley@dominiolechuga.com>
2026-05-29 03:03:41 -0300

    primeiro metodo de INSERT

--- src/dao/VeiculoDAO.java
+++ src/dao/VeiculoDAO.java
@@ -5,4 +5,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class VeiculoDAO {
+
+    public void inserirVeiculo(Veiculo veiculo) {
+        String sql = "INSERT INTO clientes (placa, modelo, marca, ano, cor, valor_diaria, disponibilidade) VALUES (?,?,?,?,?,?,?)";
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, veiculo.getVeiculoPlaca());
+            stmt.setString(2, veiculo.getVeiculoModelo());
+            stmt.setString(3, veiculo.getVeiculoMarca());
+            stmt.setInt(4, veiculo.getVeiculoAno());
+            stmt.setString(5, veiculo.getVeiculoCor());
+            stmt.setDouble(6, veiculo.getVeiculoValorDiaria());
+            stmt.setString(7, veiculo.isVeiculoDisponivel());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir o cliente: "+ e.getMessage());
+        }
+    }
 }
