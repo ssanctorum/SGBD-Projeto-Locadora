ef458a54575a2b99b69b86a0e5808dcb10052636
warlley <euwarlley@dominiolechuga.com>
2026-05-29 03:01:38 -0300

    interface do DAO

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,4 +1,12 @@
 package dao;
 
-public interface DAO {
+import java.util.List;
+
+public interface DAO<T> {
+    void inserir(T obj);
+    List<T> listarTodos();
+    void atualizar(T obj);
+    void deletar(String id);
+    void deletarPorInt(int id);
+    T buscarPorString(String id);
 }
