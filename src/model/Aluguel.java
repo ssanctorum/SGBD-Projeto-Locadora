45b6713a8667c6e54b0ded371cacf64bc7c2ffc1
Warlley Kaio dos Santos Firmino <warlley.firmino@nazaria.com.br>
2026-06-05 20:01:11 -0300

    dando inicio a implementaçao do aluguel

---
+++ src/model/Aluguel.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Aluguel {
+
+        private int id;
+        private String cpfCliente;
+        private String placaVeiculo;
+        private String dataInicio;
+        private String dataDevolucao;
+        private double valorTotal;
+        private String status;
+
+        public int getId() { return id; }
+        public String getCpfCliente() { return cpfCliente; }
+        public String getPlacaVeiculo() { return placaVeiculo; }
+        public String getDataInicio() { return dataInicio; }
+        public String getDataDevolucao() { return dataDevolucao; }
+        public double getValorTotal() { return valorTotal; }
+        public String getStatus() { return status; }
+
+        public void setId(int id) { this.id = id; }
+        public void setCpfCliente(String cpfCliente) { this.cpfCliente = cpfCliente; }
+        public void setPlacaVeiculo(String placaVeiculo) { this.placaVeiculo = placaVeiculo; }
+        public void setDataInicio(String dataInicio) { this.dataInicio = dataInicio; }
+        public void setDataDevolucao(String dataDevolucao) { this.dataDevolucao = dataDevolucao; }
+        public void setValorTotal(double valorTotal) { this.valorTotal = valorTotal; }
+        public void setStatus(String status) { this.status = status; }
+
+    // construtor sem ID e dataInicio, já que o sgbd cria em si
+    public Aluguel(String cpfCliente, String placaVeiculo, String dataDevolucao, double valorTotal) {
+        this.cpfCliente = cpfCliente;
+        this.placaVeiculo = placaVeiculo;
+        this.dataDevolucao = dataDevolucao;
+        this.valorTotal = valorTotal;
+        this.status = "ativo";
+        this.dataInicio = null;
+        this.id = 0;
+    }
+
+    // construtor com ID e dataInicio
+    public Aluguel(int id, String cpfCliente, String placaVeiculo, String dataInicio, String dataDevolucao, double valorTotal, String status) {
+        this.id = id;
+        this.cpfCliente = cpfCliente;
+        this.placaVeiculo = placaVeiculo;
+        this.dataInicio = dataInicio;
+        this.dataDevolucao = dataDevolucao;
+        this.valorTotal = valorTotal;
+        this.status = status;
+    }
+}
